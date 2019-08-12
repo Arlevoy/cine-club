cd5c068db8d118de59721c0d502538868100f3b7
Arlevoy <arthurl@bam.tech>
2019-08-12 12:52:18 +0200

    Display first page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,33 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "gatsby"
+import { Button } from "antd"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import themes from "../data/theme.json"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const renderThemes = themes => {
+  const randomThemes = themes.sort(function() {
+    return 0.5 - Math.random()
+  })
+  return randomThemes[0]
+}
+
+const IndexPage = () => {
+  const [theme, setTheme] = useState("")
+
+  const onClick = () => setTheme(renderThemes(themes))
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <div>{renderThemes(themes)}</div>
+      <Button type="primary" onClick={onClick}>
+        Choisir le thème
+      </Button>
+    </Layout>
+  )
+}
 
 export default IndexPage
